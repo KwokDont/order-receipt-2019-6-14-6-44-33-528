f97514e3edec5c2ed1a578f9ea000b9091131c8f
unknown <11059964896@qq.com>
2019-07-15 23:01:21 +0800

    [GRD] refactor-2:extract the item string to appendItemLine() function

--- src/main/java/org/katas/refactoring/OrderReceipt.java
+++ src/main/java/org/katas/refactoring/OrderReceipt.java
@@ -26,14 +26,7 @@ public class OrderReceipt {
         double totSalesTx = 0d;
         double tot = 0d;
         for (LineItem lineItem : o.getLineItems()) {
-            output.append(lineItem.getDescription());
-            output.append('\t');
-            output.append(lineItem.getPrice());
-            output.append('\t');
-            output.append(lineItem.getQuantity());
-            output.append('\t');
-            output.append(lineItem.totalAmount());
-            output.append('\n');
+            output = appendItemLine(output,lineItem);
 
             // calculate sales tax @ rate of 10%
             double salesTax = lineItem.totalAmount() * .10;
@@ -53,5 +46,17 @@ public class OrderReceipt {
 
     StringBuilder printHeaders(StringBuilder output){
         return output.append("======Printing Orders======\n");
+    }
+
+    StringBuilder appendItemLine(StringBuilder output, LineItem lineItem){
+        output.append(lineItem.getDescription());
+        output.append('\t');
+        output.append(lineItem.getPrice());
+        output.append('\t');
+        output.append(lineItem.getQuantity());
+        output.append('\t');
+        output.append(lineItem.totalAmount());
+        output.append('\n');
+        return output;
     }
 }
